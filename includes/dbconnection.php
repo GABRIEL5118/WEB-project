bfa0651e70a5967a9a4f4d025d9b9420c87900ae
GABRIEL5118 <gabrieldabush@gmail.com>
2022-12-11 20:02:53 +0200

    mytritapp

---
+++ includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost:3306", "mytritap_root", "neH;h1S(h3?_", "mytritap_trit_finalpro_db");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
